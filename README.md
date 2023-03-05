2ae9678247a1f0c672519e1e05b666316942d471
Mia <gilbertmia688@gmail.com>
2023-03-06 04:28:40 +0800

    creat tictactoe game

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# tictactoe
-用JavaScript写一个井字棋游戏
+# Tic Tac Toe
+
+![](images/tictactoe.png)
+
+## 代码逻辑
+1. 用数字**0-8**从左到右，从上到下标记3x3棋盘的每个格子。将其视为数组的index。
+    ![](images/index.png)
+2. 定义空字符串数组`option[]`代表玩家的选择，在游戏开始前，`option[]`数组里是9个空字符串：`option["", "", "", "", "", "", "", "", ""];`
+3. 定义一个`currentPlayer`，在游戏开始前默认先手是`currentPlayer = "X"`，用`changePlayer()`函数改变当前的玩家为`O`，并反复改变当前玩家。
+2. 定义`cellChecked()`函数存储玩家点击的格子的index，改变`option[index]`为`currenPlayer`。
+3. 赢下游戏的条件是[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]8种位置的字符同为`X`，或同为`O`
+    ![](images/win-condition.png)
+4. 定义`checkWinner()`函数检查在这些位置的字符是否同为`X`或同为`O`
+    - 如果相同，则得出赢家；
+    - 如果有空字符串，则继续游戏；
+    - 如果没有空字符串，且这些位置的字符都不同时等于`X`或`O`，则得出平局。
---
+++ index.js
@@ -0,0 +1,86 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+
+const winCondition =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let option = ["", "", "", "", "", "", "", "", ""]; //玩家的选择，暂时9个都为空字符
+let currentPlayer = "X";
+let running = false;
+
+startGame();
+
+function startGame(){
+    running = true;
+    cells.forEach(cell => cell.addEventListener("click", cellChecked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}' turn`;
+}
+
+function cellChecked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(option[cellIndex] !== "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+
+function updateCell(cell, index){
+    option[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner(){
+    let roundWon =false;
+    for(let i = 0; i < winCondition.length; i++){
+        const condition = winCondition[i];
+        const cellOne = option[condition[0]];
+        const cellTwo = option[condition[1]];
+        const cellThree = option[condition[2]];
+
+        if(cellOne == "" || cellTwo == "" || cellThree == ""){
+            continue;
+        }
+        if(cellOne == cellTwo && cellTwo == cellThree){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`
+        running = false;
+    }
+    else if(!option.includes("")){
+        statusText.textContent = `It's a tie`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+
+function restartGame(){
+    running = true
+    currentPlayer = "X";
+    option = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`
+    cells.forEach(cell => cell.textContent = "");
+}
